f65d0f953d44302c76b512c0719d214796b01ce3
quangnhnhut123 <quangnhnhut@gmail.com>
2023-08-02 11:48:47 +0700

    Update prompt

--- chat_hyperdb.py
+++ chat_hyperdb.py
@@ -8,11 +8,11 @@ from llama_index import (
     LangchainEmbedding,
     load_index_from_storage,
     set_global_service_context,
-    download_loader,
 )
-# from llama_hub.file.unstructured.base import UnstructuredReader
+from llama_hub.file.unstructured.base import UnstructuredReader
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings import OpenAIEmbeddings
+from hyperdb import HyperDB
 import gradio as gr
 import sys
 import os
@@ -29,7 +29,7 @@ text_embeddings_model_name = os.getenv(
 model_name = os.getenv("MODEL_NAME", "gpt-3.5-turbo")
 max_input_size = int(os.getenv("MAX_INPUT_SIZE", 4096))
 num_outputs = int(os.getenv("NUM_OUTPUTS", 1024))
-max_chunk_overlap = float(os.getenv("MAX_CHUNK_OVERLAP", 0.3))
+max_chunk_overlap = float(os.getenv("MAX_CHUNK_OVERLAP", 0.2))
 chunk_size_limit = int(os.getenv("CHUNK_SIZE_LIMIT", 600))
 temperature = float(os.getenv("TEMPERATURE", 0))
 
@@ -62,18 +62,14 @@ def init_service_context():
 
 
 def load_index(directory_path):
-    UnstructuredReader = download_loader('UnstructuredReader')
     documents = SimpleDirectoryReader(
         directory_path,
         file_extractor={
             ".pdf": UnstructuredReader(),
-            ".docx": UnstructuredReader(),
-            ".txt": UnstructuredReader(),
         },
         filename_as_id=True,
-        encoding="utf-8"
     ).load_data()
-    print(f"Loaded documents with {len(documents)} pages")
+    print(f"loaded documents with {len(documents)} pages")
 
     try:
         # Rebuild storage context
@@ -104,22 +100,54 @@ def load_index(directory_path):
 
 def data_querying(input_text, history_text):
     index = load_index("data")
-    query_engine = index.as_query_engine()
-    template = """Câu hỏi cho bạn là: {text}
-Bạn sẽ luôn trả lời bằng tiếng Việt và không đề cập đến tên tệp trong câu giới thiệu.
-Vui lòng sử dụng thông tin từ ngữ cảnh được cung cấp để trả lời chi tiết và chính xác, dễ hiểu. Cân nhắc sử dụng các số hoặc ký hiệu liệt kê."""
+    query_engine = index.as_query_engine(similarity_top_k=3)
+    template = """
+        You are an AI language model designed to provide helpful answers based on provided context.
+        You will answer in Vietnamese language and do not mention about filename in introduce sentence.
+        Please use the information from the provided context to answer accurately.
+        Ensure your answers in details with clear context for easy understanding. Consider using listing numbers or symbols.
+        Extract Kibela link from provided context then include at the end of your answer with a two-line break.
+        If there is no Kibela link in provided context, do not include reference link in your answer.
+        The question is: {text}
+    """
 
     mod_question = template.format(text=input_text)
     response = query_engine.query(mod_question)
 
     return response.response
 
+def query_hyperdb(input_text, history_text):
+    open_ai_embeddings = OpenAIEmbeddings(
+        model=text_embeddings_model_name, chunk_size=chunk_size_limit
+    )
+
+    documents = SimpleDirectoryReader(
+        "data",
+        file_extractor={
+            ".pdf": UnstructuredReader(),
+        },
+        filename_as_id=True,
+    ).load_data()
+
+    # Instantiate HyperDB with the list of documents
+    db = HyperDB(embedding_function=open_ai_embeddings)
+
+    db.add_documents(documents)
+
+    # Save the HyperDB instance to a file
+    db.save("hyperdb/demo_hyperdb.pickle.gz")
+
+    # Load the HyperDB instance from the save file
+    db.load("hyperdb/demo_hyperdb.pickle.gz")
+
+    # Query the HyperDB instance with a text input
+    results = db.query(input_text, top_k=5)
+    return results
 
 def run():
-    load_index("data")
     gr.ChatInterface(
-        fn=data_querying,
-        title="MFV-GPT INFO",
+        fn=query_hyperdb,
+        title="MFV GPT Trained By Private Data",
     ).launch(share=False)
 
 
